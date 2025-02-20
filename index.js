ae1bc78d8b5fbd34d3c5e420c16697cc240bbc65
Durjoy96 <mddurjoy853@gmail.com>
2025-02-20 16:03:28 +0600

    add MongoDB connection setup and ping confirmation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = 5000;
 require("dotenv").config();
@@ -9,6 +10,33 @@ app.use(express.json());
 app.use(cors());
 
 //routes
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.BD_PASSWORD}@cluster0.ht4sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("server working...");
 });
@@ -16,3 +44,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+run().catch(console.dir);
